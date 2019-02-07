0e92a1e0ee61ade24029d27d0d3597b393d34987
Adam Gleave <adam@gleave.me>
2019-02-07 15:34:57 -0800

    Add all the packages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from os.path import dirname, realpath
-from setuptools import setup
+from setuptools import find_packages, setup
 
 def read_requirements_file(filename):
     req_file_path = '%s/%s' % (dirname(realpath(__file__)), filename)
@@ -11,6 +11,6 @@ setup(name='gym_compete',
       version='0.0.1',
       url='https://github.com/HumanCompatibleAI/multiagent-competition',
       python_requires='>=3.6.0',
-      packages=['gym_compete'],
+      packages=find_packages(exclude=('tests',)),
       install_requires=read_requirements_file('requirements.txt'),
 )
